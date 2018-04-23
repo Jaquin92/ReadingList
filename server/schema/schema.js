92531d6647afa2b0d467270925278cceaca1efa2
Jonathan <jaquin92@yahoo.com>
2018-04-22 22:39:29 -0700

    new type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,28 +1,44 @@
 const graphql = require("graphql");
 const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
-var books = [{ name: 'name of the wind', genre: 'fantasy', id: "1" },
-{ name: 'whats up', genre: 'fantasy', id: "2" },
-{ name: 'hello world', genre: 'sci-fi', id: "3" }]
+var books =
+    [{ name: 'name of the wind', genre: 'fantasy', id: "1" },
+    { name: 'whats up', genre: 'fantasy', id: "2" },
+    { name: 'hello world', genre: 'sci-fi', id: "3" },
+    { name: 'hello world', genre: 'sci-fi', id: "4" },
+    { name: 'hello world', genre: 'sci-fi', id: "5", length: "75" }]
+
+var authors = [{ name: "Jigsaw", age: 25, id: 1 },
+{ name: "Henry", age: 21, id: 3 },
+{ name: "Hudson", age: 20, id: 2 }]
 
 
 const BookType = new GraphQLObjectType({
     name: "Book",
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
     })
 });
+
+const AuthorType = new GraphQLObjectType({
+    name: "Author",
+    fields: () => ({
+        name: { type: GraphQLString },
+        age: { type: GraphQLID },
+        id: { type: GraphQLID }
+    })
+})
 
 const RootQuery = new GraphQLObjectType({
     name: "RootQueryType",
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
 
                 return _.find(books, { id: args.id })
@@ -30,6 +46,13 @@ const RootQuery = new GraphQLObjectType({
                 //code to get data from data / other source
 
 
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { name: { type: GraphQLString } },
+            resolve(parent, args) {
+                return _.find(authors, { name: args.name })
             }
         }
     }
